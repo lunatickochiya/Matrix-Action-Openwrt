9c5464c0ab9bd90e33358c6f5fe26e3ae946d4b0
lunatickochiya <125438787@qq.com>
2023-10-14 09:52:10 +0800

    try bump easyrsa to 3.x

--- package/kochiya/luci-app-openvpn-server/root/etc/openvpncert.sh
+++ package/kochiya/luci-app-openvpn-server/root/etc/openvpncert.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 clean-all
-pkitool --initca
-build-dh
-pkitool --server server
-pkitool client1
+easyrsa init-pki
+easyrsa gen-dh
+easyrsa build-ca nopass
+easyrsa build-server-full server nopass
+easyrsa build-client-full client1 nopass
 openvpn --genkey --secret ta.key
 cp /etc/easy-rsa/keys/ca.crt /etc/openvpn/
-cp /etc/easy-rsa/keys/ca.key /etc/openvpn/
-cp /etc/easy-rsa/keys/server.crt /etc/openvpn/
-cp /etc/easy-rsa/keys/server.key /etc/openvpn/
-cp /etc/easy-rsa/keys/dh1024.pem /etc/openvpn/
-cp /etc/easy-rsa/keys/client1.crt /etc/openvpn/
-cp /etc/easy-rsa/keys/client1.key /etc/openvpn/
+cp /etc/easy-rsa/keys/private/ca.key /etc/openvpn/
+cp /etc/easy-rsa/keys/issued/server.crt /etc/openvpn/
+cp /etc/easy-rsa/keys/private/server.key /etc/openvpn/
+cp /etc/easy-rsa/keys/dh.pem /etc/openvpn/
+cp /etc/easy-rsa/keys/issued/client1.crt /etc/openvpn/
+cp /etc/easy-rsa/keys/private/client1.key /etc/openvpn/
 echo "OpenVPN Cert renew successfully"
