c5e084994d092a0c906639be4f53a30fa153ec16
lunatickochiya <125438787@qq.com>
2023-12-19 20:26:00 +0800

    add rockchip mpc1917 support private mpc1917 refine configs
    fix a chmod issue
    
    see more info

--- diy-2305.sh
+++ diy-2305.sh
@@ -10,10 +10,12 @@ version=23.05
 sed -i "52i\echo \"DISTRIB_DESCRIPTION='OpenWrt $version Compiled by 2U4U'\" >> /etc/openwrt_release" package/kochiya/autoset/files/zzz-autoset-meson
 sed -i "51i\echo \"DISTRIB_DESCRIPTION='OpenWrt $version Compiled by 2U4U'\" >> /etc/openwrt_release" package/kochiya/autoset/files/zzz-autoset-mediatek
 sed -i "51i\echo \"DISTRIB_DESCRIPTION='OpenWrt $version Compiled by 2U4U'\" >> /etc/openwrt_release" package/kochiya/autoset/files/zzz-autoset-rockchip
+sed -i "51i\echo \"DISTRIB_DESCRIPTION='OpenWrt $version Compiled by 2U4U'\" >> /etc/openwrt_release" package/kochiya/autoset/files/zzz-autoset-rockchip-siderouter
 
 grep DISTRIB_DESCRIPTION package/kochiya/autoset/files/zzz-autoset-mediatek
 grep DISTRIB_DESCRIPTION package/kochiya/autoset/files/zzz-autoset-meson
 grep DISTRIB_DESCRIPTION package/kochiya/autoset/files/zzz-autoset-rockchip
+grep DISTRIB_DESCRIPTION package/kochiya/autoset/files/zzz-autoset-rockchip-siderouter
         }
 
 function remove_error_package() {
@@ -93,14 +95,14 @@ done
 function patch_openwrt() {
         for i in $( ls mypatch ); do
             echo Applying mypatch $i
-            patch -p1 < mypatch/$i
+            patch -p1 --no-backup-if-mismatch < mypatch/$i
         done
         }
 function patch_package() {
         for packagepatch in $( ls feeds/packages/feeds-package-patch-2305 ); do
             cd feeds/packages/
             echo Applying feeds-package-patch-2305 $packagepatch
-            patch -p1 < feeds-package-patch-2305/$packagepatch
+            patch -p1 --no-backup-if-mismatch < feeds-package-patch-2305/$packagepatch
             cd ../..
         done
         }
@@ -108,7 +110,7 @@ function patch_luci() {
         for lucipatch in $( ls feeds/luci/luci-patch-2305 ); do
             cd feeds/luci/
             echo Applying luci-patch-2305 $lucipatch
-            patch -p1 < luci-patch-2305/$lucipatch
+            patch -p1 --no-backup-if-mismatch < luci-patch-2305/$lucipatch
             cd ../..
         done
         }
@@ -116,9 +118,17 @@ function patch_kiddin9() {
         for kiddin9patch in $( ls feeds/kiddin9/kiddin9-revert ); do
             cd feeds/kiddin9/
             echo Revert kiddin9 $kiddin9patch
-            patch -p1 -R < kiddin9-revert/$kiddin9patch
+            patch -p1 -R --no-backup-if-mismatch < kiddin9-revert/$kiddin9patch
             cd ../..
         done
+        }
+
+function patch_rockchip() {
+        for rockpatch in $( ls tpm312/core ); do
+            echo Applying tpm312 $rockpatch
+            patch -p1 --no-backup-if-mismatch < tpm312/core/$rockpatch
+        done
+        rm -rf tpm312
         }
 
 # add luci
@@ -139,6 +149,11 @@ mv -f package-configs/.config .config
 
 function add_mt798x_istore_packages() {
 echo "$(cat package-configs/mt798x-common-istore.config)" >> package-configs/.config
+mv -f package-configs/.config .config
+}
+
+function add_mpc1917_packages() {
+echo "$(cat package-configs/mpc1917-common.config)" >> package-configs/.config
 mv -f package-configs/.config .config
 }
 
@@ -174,6 +189,16 @@ patch_package
 patch_luci
 patch_kiddin9
 add_mt798x_istore_packages
+elif [ "$1" == "mpc1917" ]; then
+autosetver
+patch_openwrt
+remove_error_package_not_install
+patch_package
+patch_luci
+patch_kiddin9
+add_mpc1917_packages
+elif [ "$1" == "rockpatch" ]; then
+patch_rockchip
 else
 echo "Invalid argument"
 fi
--- refineconfig.sh
+++ refineconfig.sh
@@ -33,10 +33,25 @@ sed -i 's/CONFIG_PACKAGE_perl-test-harness=y/# CONFIG_PACKAGE_perl-test-harness 
 sed -i 's/CONFIG_PACKAGE_dnsmasq_full_nftset=y/# CONFIG_PACKAGE_dnsmasq_full_nftset is not set/g' .config
 }
 
+function refine_mpc1917_config() {
+sed -i 's/CONFIG_PACKAGE_firewall4=y/# CONFIG_PACKAGE_firewall4 is not set/g' .config
+sed -i 's/CONFIG_PACKAGE_nftables-json=y/# CONFIG_PACKAGE_nftables-json is not set/g' .config
+sed -i 's/CONFIG_PACKAGE_kmod-nft-offload=y/# CONFIG_PACKAGE_kmod-nft-offload is not set/g' .config
+sed -i 's/# CONFIG_PACKAGE_luci-app-passwall_Iptables_Transparent_Proxy is not set/CONFIG_PACKAGE_luci-app-passwall_Iptables_Transparent_Proxy=y/g' .config
+sed -i 's/CONFIG_PACKAGE_luci-app-passwall_Nftables_Transparent_Proxy=y/# CONFIG_PACKAGE_luci-app-passwall_Nftables_Transparent_Proxy is not set/g' .config
+sed -i 's/CONFIG_PACKAGE_dnsmasq_full_nftset=y/# CONFIG_PACKAGE_dnsmasq_full_nftset is not set/g' .config
+sed -i 's/CONFIG_PACKAGE_perl-test-harness=y/# CONFIG_PACKAGE_perl-test-harness is not set/g' .config
+sed -i 's/CONFIG_PACKAGE_dnsmasq=y/# CONFIG_PACKAGE_dnsmasq is not set/g' .config
+sed -i 's/CONFIG_PACKAGE_libnetwork=y/# CONFIG_PACKAGE_libnetwork is not set/g' .config
+sed -i 's/CONFIG_PACKAGE_qBittorrent-static=y/# CONFIG_PACKAGE_qBittorrent-static is not set/g' .config
+}
+
 if [ "$1" == "meson" ]; then
 refine_meson_config
 elif [ "$1" == "mt798x" ]; then
 refine_mt798x_config
+elif [ "$1" == "mpc1917" ]; then
+refine_mpc1917_config
 else
 echo "Invalid argument"
 fi
