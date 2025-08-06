8fa7c5ffa52e33200fbb818e4476860fb5ab0f6c
lunatickochiya <125438787@qq.com>
2025-08-06 21:58:01 +0800

    2410:fix a bug in turboacc
    Signed-off-by: lunatickochiya <125438787@qq.com>

--- add-sfe-packages.sh
+++ add-sfe-packages.sh
@@ -36,10 +36,46 @@ CONFIG_PACKAGE_kmod-shortcut-fe-cm=n
 " >> "$file"; done
 }
 
+function add_nft_config_2410() {
+for file in package-configs/*-nft-2410.config; do     echo "# ADD TURBOACC
+CONFIG_PACKAGE_luci-app-turboacc=y
+CONFIG_PACKAGE_luci-app-turboacc_INCLUDE_PDNSD=n
+
+#offload
+CONFIG_PACKAGE_kmod-nft-offload=y
+
+# sfe
+CONFIG_PACKAGE_kmod-fast-classifier=y
+CONFIG_PACKAGE_kmod-shortcut-fe=y
+CONFIG_PACKAGE_kmod-shortcut-fe-cm=n
+CONFIG_PACKAGE_kmod-nft-fullcone=y
+
+" >> "$file"; done
+}
+
+function add_ipt_config_2410() {
+for file in package-configs/*-ipt-2410.config; do     echo "# ADD TURBOACC
+CONFIG_PACKAGE_luci-app-turboacc-ipt=y
+CONFIG_PACKAGE_luci-app-turboacc_INCLUDE_PDNSD=n
+# CONFIG_PACKAGE_luci-app-fullconenat=y
+
+#offload
+CONFIG_PACKAGE_kmod-ipt-offload=y
+# sfe
+CONFIG_PACKAGE_kmod-fast-classifier=y
+CONFIG_PACKAGE_kmod-shortcut-fe=y
+CONFIG_PACKAGE_kmod-shortcut-fe-cm=n
+" >> "$file"; done
+}
+
 if [ "$1" == "nft" ]; then
 add_nft_config
 elif [ "$1" == "ipt" ]; then
 add_ipt_config
+elif [ "$1" == "ipt2410" ]; then
+add_ipt_config_2410
+elif [ "$1" == "nft2410" ]; then
+add_nft_config_2410
 else
 echo "Invalid argument"
 fi
