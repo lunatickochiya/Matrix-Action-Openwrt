4f7cb97bcc6247be76b3802857b81bf11fae026f
lunatickochiya <125438787@qq.com>
2025-09-21 20:11:17 +0800

    core:fix
    Signed-off-by: lunatickochiya <125438787@qq.com>

--- core.sh
+++ core.sh
@@ -404,7 +404,8 @@ function add_openwrt_kmods() {
 		make defconfig
 	fi
 
-	$GITHUB_WORKSPACE/$DIY_SH_RFC $Matrix_Target
+	"$GITHUB_WORKSPACE/$DIY_SH_RFC" "$Matrix_Target"
+	cd ../
 }
 
 function move_openwrt_config_ready() {
@@ -414,7 +415,8 @@ function move_openwrt_config_ready() {
 
 function fix_openwrt_feeds() {
 	cd openwrt
-	$GITHUB_WORKSPACE/$DIY_SH $Matrix_Target
+	"$GITHUB_WORKSPACE/$DIY_SH"  "$Matrix_Target"
+	cd ../
 }
 
 function fix_openwrt_nss_sfe_feeds() {
@@ -426,6 +428,7 @@ function fix_openwrt_nss_sfe_feeds() {
 }
 
 function refine_openwrt_config() {
+	cd openwrt
 	IFS=',' read -r -a package_array <<< "$INPUT_PKGS_CFG_FOO"
 	for pkg in "${package_array[@]}"; do
 		./scripts/feeds install "$pkg"
@@ -448,7 +451,8 @@ function refine_openwrt_config() {
 		awk -v pkg="$pkg" '\$0 ~ pkg { print }' .config
 	done
 
-	$GITHUB_WORKSPACE/$DIY_SH_RFC $Matrix_Target
+	"$GITHUB_WORKSPACE/$DIY_SH_RFC" "$Matrix_Target"
+	cd ../
 }
 
 function awk_openwrt_config() {
